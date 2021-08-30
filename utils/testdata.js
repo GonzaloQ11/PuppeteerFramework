169cb578535af5f225ec3323c6e0155c275f2234
Gonzalo Quispe <gonzalofquispe@gmail.com>
2021-08-30 00:54:51 -0300

    remove double quotes from properties

--- utils/testdata.js
+++ utils/testdata.js
@@ -1,21 +1,21 @@
 export const testdata = {
-    "user": {
-        "username": "Admin",
-        "password": "admin123",
-        "invalidPassword": "123456",
-        "employeeName": "Paul Collings"
+    user: {
+        username: "Admin",
+        password: "admin123",
+        invalidPassword: "123456",
+        employeeName: "Paul Collings"
     },
-    "roles": {
-        "ess": "ESS",
-        "admin": "Admin"
+    roles: {
+        ess: "ESS",
+        admin: "Admin"
     },
-    "statuses": {
-        "enabled": "Enabled",
-        "disabled": "Disabled"
+    statuses: {
+        enabled: "Enabled",
+        disabled: "Disabled"
     },
-    "errorMessages": {
-        "emptyUsername": "Username cannot be empty",
-        "emptyPassword": "Password cannot be empty",
-        "invalidCredentials": "Invalid credentials",
+    errorMessages: {
+        emptyUsername: "Username cannot be empty",
+        emptyPassword: "Password cannot be empty",
+        invalidCredentials: "Invalid credentials",
     }
 }
